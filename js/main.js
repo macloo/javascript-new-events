6911aa1afb2d7d32334c8363d74018b470de69e1
macloo <mmcadams@gmail.com>
2019-02-21 21:15:56 -0500

    fix comments

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // event handler for #clicky and .brown
 document.querySelector('#clicky').onclick = () => {
-    // the next line hides any element that has class="brown"
+    // the next line hides the element that has class="brown"
     document.querySelector('.brown').style.display = 'none';
-    // the next line changes the bg of any element that has class="second"
+    // the next line changes the bg of the element that has class="second"
     document.querySelector('.second').style.background = '#fcd08d';
 }
 
